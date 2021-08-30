a908ecbeec93cc09d03d5f873d0f9b6d1265d408
Yana <yankayanka4815@gmail.com>
2021-08-31 01:22:19 +0300

    Create models of data

---
+++ CourseTests/Entities/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseTests.Entities
+{
+    public class Question
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public PossibleAnswer PossibleAnswer { get; set; }
+        public Test Test { get; set; }
+
+    }
+}
