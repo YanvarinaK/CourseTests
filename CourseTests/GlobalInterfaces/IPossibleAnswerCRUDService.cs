d3ecaad0404aa80b289ab35e3a5d77bbdc66643d
Yana <yankayanka4815@gmail.com>
2021-08-31 17:58:46 +0300

    Create CRUD Interfaces

---
+++ CourseTests/GlobalInterfaces/IPossibleAnswerCRUDService.cs
@@ -0,0 +1,22 @@
+﻿using CourseTests.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseTests.GlobalInterfaces
+{
+    public interface IPossibleAnswerCRUDService
+    {
+        bool Create(PossibleAnswer entity);
+
+        PossibleAnswer Get(Guid id);
+
+        List<PossibleAnswer> List();
+
+        bool Update(PossibleAnswer newEntity, Guid id);
+
+        bool Delete(Guid id);
+    }
+}
---
+++ CourseTests/GlobalInterfaces/IRecordAnswerCRUDService.cs
@@ -0,0 +1,22 @@
+﻿using CourseTests.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseTests.GlobalInterfaces
+{
+    public interface IRecordAnswerCRUDService
+    {
+        bool Create(RecordAnswer entity);
+
+        RecordAnswer Get(Guid id);
+
+        List<RecordAnswer> List();
+
+        bool Update(RecordAnswer newEntity, Guid id);
+
+        bool Delete(Guid id);
+    }
+}
