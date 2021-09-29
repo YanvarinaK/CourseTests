4c34a2df774da5b29890ac7d820be30e78fdf909
Yana <yankayanka4815@gmail.com>
2021-09-29 22:16:48 +0300

    Create SQL Views

---
+++ CourseTests/DB_Scripts/ViewListOfWrongAnswers.sql
@@ -0,0 +1,8 @@
+CREATE VIEW ListOfWrongAnswers AS
+SELECT PossibleAnswers.Id AS RightAnswerId,
+	   PossibleAnswers.Name AS RightAnswerName,
+	   PossibleAnswers.QuestionId AS QuestionID,
+	   Questions.Name AS QuestionName
+FROM CourseTestsDB.dbo.PossibleAnswers
+INNER JOIN CourseTestsDB.dbo.Questions ON Questions.Id = PossibleAnswers.QuestionId
+WHERE PossibleAnswers.IsRight = 0;
